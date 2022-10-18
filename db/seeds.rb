eae105942b726f11d6780416c7965c2666f8023d
Chris Siku <chrissiku5@gmail.com>
2022-10-18 16:46:32 +0200

    Add database valifation and seed the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# Greet 5 random celebrities.
+5.times do
+    Greeting.create(message: "Hello, #{Faker::Name.name}!")
+end
